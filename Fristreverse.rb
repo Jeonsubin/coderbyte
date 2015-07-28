fc9375c42e44667e9028e8a5fc060c54d63e1455
Jeonsubin <7subin7@naver.com>
2015-07-28 10:51:17 +0900

    Create Fristreverse.rb

---
+++ Fristreverse.rb
@@ -0,0 +1,11 @@
+def FirstReverse(str)
+# code goes here
+# str  문장 뒤집기
+
+return str.reverse 
+
+end 
+
+#keep this function call here
+#to see how to enter arguments in Ruby scroll down
+FirstReverse[SIDIN.gets]
